a9be4fbafee12a9a5b884e75af43b883c3077ebe
E <emmzie84@gmail.com>
2013-04-12 12:35:48 +0200

    Added DBAdapter
    Added an common DBAdapter so the database can be used in the
    application.

---
+++ src/com/example/exjobb/DrugsDBAdapter.java
@@ -0,0 +1,12 @@
+package com.example.exjobb;
+
+public class DrugsDBAdapter {
+
+	public static String DRUG_NAME;
+	public static String TYPE;
+	public static String POTENCY;
+	public static String SIZE;
+	public static String PREFERENTIAL_PRICE;
+	public static String PRESCRIPTION_ONLY;
+
+}
---
+++ src/com/example/exjobb/StockDBAdapter.java
@@ -0,0 +1,10 @@
+package com.example.exjobb;
+
+public class StockDBAdapter {
+
+	public static String DRUG_ID; //Trots att det är en INTEGER???
+	public static String PHARMACY_ID; //Trots att det är en INTEGER???
+	public static String NUMBER; //Trots att det är en INTEGER???
+	public static String PRICE; //Trots att det är en REAL???
+
+}
