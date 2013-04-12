af1db42960d578540c6bec732964dc1dfb661ff4
E <emmzie84@gmail.com>
2013-04-12 12:55:10 +0200

    Added PharmaciesDBAdapter
    See DBAdapter

--- src/com/example/exjobb/DBAdapter.java
+++ src/com/example/exjobb/DBAdapter.java
@@ -12,5 +12,19 @@ public class DBAdapter {
 			+ DrugsDBAdapter.PREFERENTIAL_PRICE + " TEXT,"
 			+ DrugsDBAdapter.PRESCRIPTION_ONLY + " TEXT" + ");";
 	
-	
+	private static final String CREATE_TABLE_PHARMACIES = "create table pharmacies (_id integer primary key autoincrement, "
+			+ PharmaciesDBAdapter.CHAIN_NAME + " TEXT,"
+			+ PharmaciesDBAdapter.PHARMACY_NAME + " TEXT,"
+			+ PharmaciesDBAdapter.ADDRESS + " TEXT,"
+			+ PharmaciesDBAdapter.POSTAL_CODE + " INTEGER,"
+			+ PharmaciesDBAdapter.POSTAL_AREA + " TEXT,"
+			+ PharmaciesDBAdapter.LATITUDE + " TEXT,"
+			+ PharmaciesDBAdapter.LONGITUDE + " TEXT,"
+			+ PharmaciesDBAdapter.PHONE_NBR + " TEXT,"
+			+ PharmaciesDBAdapter.OPENING_HOURS_WD + " TEXT,"
+			+ PharmaciesDBAdapter.CLOSING_HOURS_WD + " TEXT,"
+			+ PharmaciesDBAdapter.OPENING_HOURS_SAT + " TEXT,"
+			+ PharmaciesDBAdapter.CLOSING_HOURS_SAT + " TEXT,"
+			+ PharmaciesDBAdapter.OPENING_HOURS_SUN + " TEXT,"
+			+ PharmaciesDBAdapter.CLOSING_HOURS_SUN + " TEXT" + ");";
 }
