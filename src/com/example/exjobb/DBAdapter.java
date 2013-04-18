806a1fb554cd2245845840e25937d56fc7daf162
E <emmzie84@gmail.com>
2013-04-18 12:35:01 +0200

    Finished DBAdapter & DrugsDBAdapter

--- src/com/example/exjobb/DBAdapter.java
+++ src/com/example/exjobb/DBAdapter.java
@@ -1,5 +1,10 @@
 package com.example.exjobb;
 
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 public class DBAdapter {
 	public static final String DATABASE_NAME = "mydb";
 	public static final int DATABASE_VERSION = 1;
@@ -33,4 +38,42 @@ public class DBAdapter {
 			+ StockDBAdapter.PHARMACY_ID + " INTEGER,"
 			+ StockDBAdapter.NUMBER + " INTEGER,"
 			+ StockDBAdapter.PRICE + " INTEGER" + ");";
+	
+	private final Context context;
+	private DatabaseHelper DBHelper;
+	private SQLiteDatabase db;
+	
+	public DBAdapter(Context ctx) {
+		this.context = ctx;
+		this.DBHelper = new DatabaseHelper(this.context);
+	}
+	
+	public static class DatabaseHelper extends SQLiteOpenHelper {
+		DatabaseHelper(Context context) {
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			db.execSQL(CREATE_TABLE_DRUGS);
+			db.execSQL(CREATE_TABLE_PHARMACIES);
+			db.execSQL(CREATE_TABLE_STOCK);
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			// TODO Auto-generated method stub
+		}
+		
+	}
+	
+	public DBAdapter open() throws SQLException {
+		this.db = this.DBHelper.getWritableDatabase();
+		return this;
+	}
+	
+	public void close() {
+		this.DBHelper.close();
+	}
+		
 }
--- src/com/example/exjobb/DrugsDBAdapter.java
+++ src/com/example/exjobb/DrugsDBAdapter.java
@@ -1,12 +1,61 @@
 package com.example.exjobb;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.exjobb.DBAdapter.DatabaseHelper;
+
 public class DrugsDBAdapter {
+	public static final String ROW_ID = "_id";
+	public static final String DRUG_NAME = "drug_name";
+	public static final String TYPE = "type";
+	public static final String POTENCY = "potency";
+	public static final String SIZE = "size";
+	public static final String PREFERENTIAL_PRICE = "preferential_price";
+	public static final String PRESCRIPTION_ONLY = "prescription_only";
+	
+	private static final String DATABASE_TABLE = "Drugs";
+	
+	private DatabaseHelper mDBHelper;
+	private SQLiteDatabase mDB;
+	private final Context mCtx;
 
-	public static String DRUG_NAME;
-	public static String TYPE;
-	public static String POTENCY;
-	public static String SIZE;
-	public static String PREFERENTIAL_PRICE;
-	public static String PRESCRIPTION_ONLY;
+	private static class DatabaseHelper extends SQLiteOpenHelper {
+		DatabaseHelper(Context context) {
+			super(context, DBAdapter.DATABASE_NAME, null, DBAdapter.DATABASE_VERSION);
+		}
+		
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			// TODO Auto-generated method stub
+			
+		}
 
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	
+	public DrugsDBAdapter(Context ctx) {
+		this.mCtx = ctx;
+	}
+	
+	public DrugsDBAdapter open() throws SQLException {
+		this.mDBHelper = new DatabaseHelper(this.mCtx);
+		this.mDB = this.mDBHelper.getWritableDatabase();
+		return this;
+	}
+	
+	public void close() {
+		this.mDBHelper.close();
+	}
+	
+	public Cursor getAllDrugs() {
+		return this.mDB.query(DATABASE_TABLE, new String[] {ROW_ID, DRUG_NAME, TYPE, POTENCY, SIZE, PREFERENTIAL_PRICE, PRESCRIPTION_ONLY}, null, null, null, null, null);
+	}
 }
